21e153c0892a6cdbd71afd3f2a1aa3b6749d78f4
akshay23chaudhary <150373226+akshay23chaudhary@users.noreply.github.com>
2026-06-08 22:00:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Leap Year/leap-year.c
@@ -0,0 +1,11 @@
+// User function Template for C
+
+int checkYear(int n) {
+    // code here
+    if(n%400==0)
+    return 1;
+    else if(n%4==0&&n%100!=0)
+    return 1;
+    else
+    return 0;
+}
